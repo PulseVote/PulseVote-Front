f6ad877f52bf4147d3df178b45fd20024f94ba43
ShravanRamjathan <shravanramjathan@gmail.com>
2025-07-24 22:28:00 +0200

    Fix useEffect dependency array in DisplayTestData component

--- src/components/testData.jsx
+++ src/components/testData.jsx
@@ -1,12 +1,13 @@
 import { useState, useEffect } from "react";
 import { fetchTest } from "../api/testApi.js";
+
 export default function DisplayTestData() {
   const [serverMessage, setMessage] = useState("Loading...");
   useEffect(() => {
     fetchTest().then((res) => {
       setMessage(res);
     });
-  }, []);
+  });
   return (
     <div>
       <h2>{serverMessage}</h2>
