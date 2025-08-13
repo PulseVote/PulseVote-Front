4a2ad4f402a435a1440aa0675b0f5114e56dcbe4
ShravanRamjathan <shravanramjathan@gmail.com>
2025-08-13 21:23:12 +0200

    feat: :sparkles: Added safety on axios

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -2,4 +2,10 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: `https://localhost:3000/`,
   timeout: 10000,
+  withCredentials: true,
 });
+const accessToken = null;
+function getAccessToken(token) {
+  accessToken = token;
+}
+api.interceptors.request;
